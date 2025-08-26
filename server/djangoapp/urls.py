7a93fd72033770a20d4bf59c6df69da072b6dd84
Warren Hunt <wmatthunt@gmail.com>
2025-08-26 12:24:47 -0400

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -16,9 +16,9 @@ urlpatterns = [
     # path for dealer reviews view
     path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
     path(route='get_dealers/<str:state>', view=views.get_dealerships,
-        name='get_dealers_by_state'),
-    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, 
-     name='dealer_details'),
+         name='get_dealers_by_state'),
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews,
+         name='dealer_details'),
 
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details,
          name='dealer_details'),
@@ -26,11 +26,7 @@ urlpatterns = [
     # path for add a review view
     path(route='add_review', view=views.add_review, name='add_review'),
 
-    #path for get cars
+    # path for get cars
     path(route='get_cars', view=views.get_cars, name ='getcars'),
-
-
-
-
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
