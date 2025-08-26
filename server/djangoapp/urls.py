470d4572506b324d55359c124d14a44a77b38dde
Warren Hunt <wmatthunt@gmail.com>
2025-08-26 12:26:05 -0400

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -27,6 +27,6 @@ urlpatterns = [
     path(route='add_review', view=views.add_review, name='add_review'),
 
     # path for get cars
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
+    path(route='get_cars', view=views.get_cars, name='getcars'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
