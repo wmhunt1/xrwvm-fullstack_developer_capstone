356dafbf55e872b37b1eb1fb73c5de0066229e79
Warren Hunt <wmatthunt@gmail.com>
2025-08-26 12:20:37 -0400

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -7,16 +7,18 @@ from . import views
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
-   path('register', views.registration, name='register'),
-
+    path('register', views.registration, name='register'),
+    
     # path for login
     path(route='login', view=views.login_user, name='login'),
     path(route='logout', view=views.logout_request, name='logout'),
 
     # path for dealer reviews view
     path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
-    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
-    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, 
+     name='get_dealers_by_state'),
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, 
+     name='dealer_details'),
 
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
 
